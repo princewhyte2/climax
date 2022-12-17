7bbd5cc8afb5799e1a750da54c76edf4dffb0b99
Simeon Kenneth <simeonkenneth3@gmail.com>
2022-12-17 04:34:37 +0100

    update manifest and favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,39 +1,42 @@
-import Head from "next/head"
-import "../styles/globals.css"
-import { AppProps } from "next/app"
-import { useEffect } from "react"
-import Pusher from "pusher-js"
+import Head from "next/head";
+import "../styles/globals.css";
+import { AppProps } from "next/app";
+import { useEffect } from "react";
+import Pusher from "pusher-js";
 
-import runOneSignal from "../services/onesignal"
-import { SessionProvider } from "next-auth/react"
-import useClimaxStores from "../lib/store"
+import runOneSignal from "../services/onesignal";
+import { SessionProvider } from "next-auth/react";
+import useClimaxStores from "../lib/store";
 
-export default function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
-  const setPusher = useClimaxStores((state: any) => state.setPusher)
-  const pusher = useClimaxStores((state: any) => state.pusher)
-  const setChannel = useClimaxStores((state: any) => state.setChannel)
-  const channel = useClimaxStores((state: any) => state.channel)
-  const test = useClimaxStores((state: any) => state.test)
+export default function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  const setPusher = useClimaxStores((state: any) => state.setPusher);
+  const pusher = useClimaxStores((state: any) => state.pusher);
+  const setChannel = useClimaxStores((state: any) => state.setChannel);
+  const channel = useClimaxStores((state: any) => state.channel);
+  const test = useClimaxStores((state: any) => state.test);
   useEffect(() => {
-    runOneSignal()
-  }, [])
+    runOneSignal();
+  }, []);
 
   useEffect(() => {
     setPusher(
       new Pusher("fbc75136973291e9f2cc", {
         cluster: "eu",
-      }),
-    )
-  }, [])
+      })
+    );
+  }, []);
   useEffect(() => {
-    setChannel(pusher?.subscribe("my-channel"))
-  }, [pusher])
+    setChannel(pusher?.subscribe("my-channel"));
+  }, [pusher]);
 
   useEffect(() => {
     channel?.bind("my-event", function (data: any) {
-      alert(JSON.stringify(data))
-    })
-  }, [channel])
+      alert(JSON.stringify(data));
+    });
+  }, [channel]);
 
   return (
     <>
@@ -46,17 +49,34 @@ export default function MyApp({ Component, pageProps: { session, ...pageProps } 
         />
         <meta name="description" content="Description" />
         <meta name="keywords" content="Keywords" />
-        <title>Next.js PWA Example</title>
+        <title>Climax | Be aware and make the change</title>
 
         <link rel="manifest" href="/manifest.json" />
-        <link href="/icons/favicon-16x16.png" rel="icon" type="image/png" sizes="16x16" />
-        <link href="/icons/favicon-32x32.png" rel="icon" type="image/png" sizes="32x32" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
         <link rel="apple-touch-icon" href="/apple-icon.png"></link>
-        <meta name="theme-color" content="#317EFB" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#2d89ef" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <SessionProvider session={session}>
         <Component {...pageProps} />
       </SessionProvider>
     </>
-  )
+  );
 }
